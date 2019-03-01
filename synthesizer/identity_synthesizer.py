f027d7d690112303742a95d459cf4edbcdd4dd0e
Lei Xu <leix@mit.edu>
2019-03-01 11:57:17 -0500

    First naive synthesizer

--- synthesizer/identity_synthesizer.py
+++ synthesizer/identity_synthesizer.py
@@ -1,10 +1,20 @@
-from synthesizer_base import Synthesizer
+from synthesizer_base import SynthesizerBase, run
 
-class IdentitySynthesizer(Synthesizer):
+class IdentitySynthesizer(SynthesizerBase):
     """docstring for IdentitySynthesizer."""
 
-    def train(self, train_data, meta):
+    supported_datasets = []
+
+    def train(self, train_data):
         self.learned = train_data
 
-    def generate(self, n, meta):
+    def generate(self, n):
+        assert len(self.learned == n)
+        return [(0, self.learned)]
+
+    def init(self, datset, meta):
         pass
+
+
+if __name__ == "__main__":
+    run(IdentitySynthesizer())
--- synthesizer/synthesizer_base.py
+++ synthesizer/synthesizer_base.py
@@ -1,8 +1,93 @@
-class Synthesizer(object):
+import argparse
+import logging
+import glob
+import os
+import json
+import numpy as np
+
+
+logging.basicConfig(level=logging.INFO)
+
+parser = argparse.ArgumentParser(description='Launch one synthesizer on one or multiple datasets.')
+parser.add_argument('datasets', type=str, nargs='*',
+                    help='a list of datasets, empty means all datasets.')
+parser.add_argument('--repeat', type=int, default=3,
+                    help='run generater multiple times.')
+
+parser.add_argument('--output', type=str, default='output',
+                    help='output dir')
+parser.add_argument('--name', type=str, default='',
+                    help='model name, default is model class name.')
+
+class SynthesizerBase(object):
     """docstring for Synthesizer."""
 
-    def train(self, train_data, meta):
+    def train(self, train_data):
         pass
 
-    def generate(self, n, meta):
+    def generate(self, n):
         pass
+
+    def init(self, meta, working_dir):
+        pass
+
+
+def run(synthesizer):
+    assert isinstance(synthesizer, SynthesizerBase)
+
+    args = parser.parse_args()
+    datasets = args.datasets
+    name = args.name
+    if name == "":
+        name = synthesizer.__class__.__name__
+
+    output = "{}/{}".format(args.output, name)
+
+    if not os.path.exists(output):
+        os.makedirs(output)
+    logging.info("Use output dir {}".format(output))
+
+    repeat = args.repeat
+
+    if datasets == []:
+        datasets = synthesizer.supported_datasets
+    else:
+        for item in datasets:
+            if not item in synthesizer.supported_datasets:
+                logging.warning("Dataset {} is not supported by {}.".format(item, name))
+
+
+
+    for dataset in datasets:
+        if glob.glob("{}/{}*.npz".format(output, dataset)):
+            logging.warning("Skip. {} results on {} exists.".format(name, dataset))
+            continue
+
+
+        data_filename = glob.glob("data/*/{}.npz".format(dataset))
+        meta_filename = glob.glob("data/*/{}.json".format(dataset))
+
+        if len(data_filename) != 1:
+            logging.warning("Skip. Can't find dataset {}. ".format(dataset))
+            continue
+
+        if len(meta_filename) != 1:
+            logging.warning("Skip. Can't find meta {}. ".format(dataset))
+            continue
+
+        data = np.load(data_filename[0])['train']
+        with open(meta_filename[0]) as f:
+            meta = json.load(f)
+
+
+        for i in range(repeat):
+            working_dir = "{}/ckpt_{}_{}".format(output, dataset, i)
+            synthesizer.init(meta, working_dir)
+            synthesizer.train(data)
+            generated = synthesizer.generate(data.shape[0])
+
+            if len(generated) == 0:
+                logging.warning("{} fails on {}. ".format(name, dataset))
+
+            for step, syn in generated:
+                np.savez("{}/{}_{}_{}.npz".format(output, dataset, i, step), syn=syn)
