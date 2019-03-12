cd1e54a1657a81b30e5a21204e8a531d17529282
Alicia Sun <alicia@Alicias-MacBook-Pro-3.local>
2019-03-12 17:35:42 -0400

    add bayesian samples

--- sdata_maker/bivariate.py
+++ sdata_maker/bivariate.py
@@ -1,8 +1,9 @@
+import argparse
 import itertools
 import json
+import math
 import os
 import sys
-from math import sqrt
 
 import numpy as np
 from sklearn.datasets import make_circles
@@ -31,7 +32,7 @@ def make_gaussian_mixture(dist_type, num_samples, num_components = 25, s = 0.05,
         mus = np.array([np.array([i, j]) for i, j in itertools.product(range(-4, 5, 2),
                                                         range(-4, 5, 2))],dtype=np.float32)
     elif dist_type == "gaussian_ring":
-        mus = np.array([[-1,0],[1,0],[0,-1],[0,1],[-sqrt(1/2),-sqrt(1/2)],[sqrt(1/2),sqrt(1/2)],[-sqrt(1/2),sqrt(1/2)],[sqrt(1/2),-sqrt(1/2)]])
+        mus = np.array([[-1,0],[1,0],[0,-1],[0,1],[-math.sqrt(1/2),-math.sqrt(1/2)],[math.sqrt(1/2),math.sqrt(1/2)],[-math.sqrt(1/2),math.sqrt(1/2)],[math.sqrt(1/2),-math.sqrt(1/2)]])
 
     for i in range(num_components):
         if (i+1)*bsize >= num_samples:
@@ -41,22 +42,26 @@ def make_gaussian_mixture(dist_type, num_samples, num_components = 25, s = 0.05,
     return samples
 
 def make_two_rings(num_samples):
-    samples, labels = make_circles(num_samples, shuffle=True, noise=None, random_state=None, factor=0.8)
+    samples, labels = make_circles(num_samples, shuffle=True, noise=None, random_state=None, factor=0.6)
     return samples
 
 if __name__ == "__main__":
-    output_dir = "data/real"
+    parser = argparse.ArgumentParser(description='Generate Synthetic Data for a distribution')
+    parser.add_argument('distribution', type = str, help = 'specify type of distributions to sample from')
+    parser.add_argument('--sample', type=int, default=10000,
+                    help='maximum samples in the synthetic data.')
+    args = parser.parse_args()
+    dist = args.distribution
+    num_sample = args.sample
+    samples = create_distribution(dist, num_sample*2)
+
+    output_dir = "data/synthetic"
     try:
         os.mkdir(output_dir)
     except:
         pass
-    if len(sys.argv) != 3:
-        raise ValueError("Please specify the name of the distribution and number of samples to generate")
 
-    dist_type = sys.argv[1]
-    num_samples = int(sys.argv[2])
-
-    samples = create_distribution(dist_type, num_samples*2)
+    
 
     # Store Meta Files
     meta = []
@@ -68,9 +73,9 @@ if __name__ == "__main__":
                 "max": np.max(samples[:,i].astype('float'))
         })
     # Store synthetic data
-    with open("{}/{}.json".format(output_dir, dist_type), 'w') as f:
+    with open("{}/{}.json".format(output_dir, dist), 'w') as f:
         json.dump(meta, f, sort_keys=True, indent=4, separators=(',', ': '))
-    np.savez("{}/{}.npz".format(output_dir, dist_type), train=samples[:len(samples)//2], test=samples[len(samples)//2:])
+    np.savez("{}/{}.npz".format(output_dir, dist), train=samples[:len(samples)//2], test=samples[len(samples)//2:])
 
 
 
