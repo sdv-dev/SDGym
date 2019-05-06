8328d0b10ba89f573f2c5b45c66838a58cda823e
Lei Xu <leix@mit.edu>
2019-05-06 14:58:13 -0400

    remove scripts.

--- eva.sh
+++
@@ -1,6 +0,0 @@
-export CUDA_VISIBLE_DEVICES=-1
-rm output/__result__/*WGAN3
-python3 launcher.py bgmwgan3 --dataset chain --repeat 1 &
-# python3 launcher.py bgmwgan2 --dataset chain --repeat 1 &
-# python3 launcher.py bgmgan4 --dataset chain --repeat 1 &
-# python3 launcher.py bgmgan5 --dataset chain --repeat 1 &
--- run.sh
+++
@@ -1,8 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-python3 launcher.py bgmwgan3 --datasets grid ring fc general chain tree --repeat 3 &
-python3 launcher.py bgmwgan3 --datasets mnist12 --repeat 3 &
-python3 launcher.py bgmwgan3 --datasets mnist28 --repeat 3 &
-export CUDA_VISIBLE_DEVICES=1
-python3 launcher.py bgmwgan3 --datasets news --repeat 3 &
-python3 launcher.py bgmwgan3 --datasets adult --repeat 3 &
-python3 launcher.py bgmwgan3 --datasets credit --repeat 3 &
