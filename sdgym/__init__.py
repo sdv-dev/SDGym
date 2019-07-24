6936e9da8127bbabbeb6577671b25a86a21d77cd
Carles Sala <c.sala.stq@gmail.com>
2019-07-24 10:32:53 +0200

    Fix imports

--- sdgym/__init__.py
+++ sdgym/__init__.py
@@ -4,9 +4,14 @@ SDGym is a framework to benchmark the performance of synthetic data generators f
 non-temporal tabular data.
 """
 
-
 __author__ = 'MIT Data To AI Lab'
 __copyright__ = 'Copyright (c) 2018, MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com'
 __license__ = 'MIT'
 __version__ = '0.1.0-dev'
+
+from sdgym.benchmark import benchmark
+
+__all__ = [
+    'benchmark'
+]
