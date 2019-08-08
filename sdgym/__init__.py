a559ec62218a84bdd19eec93f78e78f8220bc7c8
Carles Sala <carles@pythiac.com>
2019-08-08 10:52:28 +0200

    Bump version: 0.1.0-dev → 0.1.0

--- sdgym/__init__.py
+++ sdgym/__init__.py
@@ -8,7 +8,7 @@ __author__ = 'MIT Data To AI Lab'
 __copyright__ = 'Copyright (c) 2018, MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com'
 __license__ = 'MIT'
-__version__ = '0.1.0-dev'
+__version__ = '0.1.0'
 
 from sdgym.benchmark import benchmark
 from sdgym.data import load_dataset
