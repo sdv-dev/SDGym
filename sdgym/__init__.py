de3b8943815dc07d4b11ba9747470ab7315d1a62
Carles Sala <carles@pythiac.com>
2020-04-10 21:22:39 +0200

    Bump version: 0.2.0.dev1 → 0.2.0.dev2

--- sdgym/__init__.py
+++ sdgym/__init__.py
@@ -8,7 +8,7 @@ __author__ = 'MIT Data To AI Lab'
 __copyright__ = 'Copyright (c) 2018, MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com'
 __license__ = 'MIT'
-__version__ = '0.2.0.dev1'
+__version__ = '0.2.0.dev2'
 
 from sdgym.benchmark import benchmark
 from sdgym.data import load_dataset
--- setup.py
+++ setup.py
@@ -92,6 +92,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDGym',
-    version='0.2.0.dev1',
+    version='0.2.0.dev2',
     zip_safe=False,
 )
