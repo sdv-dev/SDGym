827272b877661d65cc91dc055799c53682834c41
Carles Sala <carles@pythiac.com>
2020-10-17 13:08:13 +0200

    Bump version: 0.2.2 → 0.2.3.dev0

--- sdgym/__init__.py
+++ sdgym/__init__.py
@@ -8,7 +8,7 @@ __author__ = 'MIT Data To AI Lab'
 __copyright__ = 'Copyright (c) 2018, MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com'
 __license__ = 'MIT'
-__version__ = '0.2.2'
+__version__ = '0.2.3.dev0'
 
 from sdgym import benchmark, results, synthesizers
 from sdgym.benchmark import run
