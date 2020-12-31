acbec1a14515dd117ad5454b0a1081a433e074d3
Carles Sala <carles@pythiac.com>
2020-12-31 16:26:09 +0100

    Use temporary dir for privbn

--- sdgym/synthesizers/privbn.py
+++ sdgym/synthesizers/privbn.py
@@ -1,7 +1,9 @@
 import logging
 import os
+import pathlib
 import shutil
 import subprocess
+import tempfile
 from datetime import datetime
 
 import numpy as np
@@ -31,52 +33,54 @@ class PrivBN(LegacySingleTableBaseline):
         self.meta = Transformer.get_metadata(data, categorical_columns, ordinal_columns)
 
     def sample(self, n):
-        try_mkdirs("__privbn_tmp/data")
-        try_mkdirs("__privbn_tmp/log")
-        try_mkdirs("__privbn_tmp/output")
-        shutil.copy("privbayes/privBayes.bin", "__privbn_tmp/privBayes.bin")
-        d_cols = []
-        with open("__privbn_tmp/data/real.domain", "w") as f:
-            for id_, info in enumerate(self.meta):
-                if info['type'] in [CATEGORICAL, ORDINAL]:
-                    print("D", end='', file=f)
-                    counter = 0
-                    for i in range(info['size']):
-                        if i > 0 and i % 4 == 0:
-                            counter += 1
-                            print(" {", end='', file=f)
-                        print("", i, end='', file=f)
-                    print(" }" * counter, file=f)
-                    d_cols.append(id_)
-                else:
-                    minn = info['min']
-                    maxx = info['max']
-                    d = (maxx - minn) * 0.03
-                    minn = minn - d
-                    maxx = maxx + d
-                    print("C", minn, maxx, file=f)
-
-        with open("__privbn_tmp/data/real.dat", "w") as f:
-            n = len(self.data)
-            np.random.shuffle(self.data)
-            n = min(n, self.max_samples)
-            for i in range(n):
-                row = self.data[i]
-                for id_, col in enumerate(row):
-                    if id_ in d_cols:
-                        print(int(col), end=' ', file=f)
-
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmpdir = pathlib.Path(tmpdir)
+            try_mkdirs(tmpdir / "data")
+            try_mkdirs(tmpdir / "log")
+            try_mkdirs(tmpdir / "output")
+            shutil.copy("privbayes/privBayes.bin", tmpdir / "privBayes.bin")
+            d_cols = []
+            with open(tmpdir / "data/real.domain", "w") as f:
+                for id_, info in enumerate(self.meta):
+                    if info['type'] in [CATEGORICAL, ORDINAL]:
+                        print("D", end='', file=f)
+                        counter = 0
+                        for i in range(info['size']):
+                            if i > 0 and i % 4 == 0:
+                                counter += 1
+                                print(" {", end='', file=f)
+                            print("", i, end='', file=f)
+                        print(" }" * counter, file=f)
+                        d_cols.append(id_)
                     else:
-                        print(col, end=' ', file=f)
+                        minn = info['min']
+                        maxx = info['max']
+                        d = (maxx - minn) * 0.03
+                        minn = minn - d
+                        maxx = maxx + d
+                        print("C", minn, maxx, file=f)
 
-                print(file=f)
+            with open(tmpdir / "data/real.dat", "w") as f:
+                n = len(self.data)
+                np.random.shuffle(self.data)
+                n = min(n, self.max_samples)
+                for i in range(n):
+                    row = self.data[i]
+                    for id_, col in enumerate(row):
+                        if id_ in d_cols:
+                            print(int(col), end=' ', file=f)
 
-        privbayes = os.path.realpath("__privbn_tmp/privBayes.bin")
-        arguments = [privbayes, "real", str(n), "1", str(self.theta)]
-        LOGGER.info('Calling %s', ' '.join(arguments))
-        start = datetime.utcnow()
-        subprocess.call(arguments, cwd="__privbn_tmp")
-        LOGGER.info('Elapsed %s', datetime.utcnow() - start)
+                        else:
+                            print(col, end=' ', file=f)
 
-        return np.loadtxt(
-            "__privbn_tmp/output/syn_real_eps10_theta{}_iter0.dat".format(self.theta))
+                    print(file=f)
+
+            privbayes = os.path.realpath(tmpdir / "privBayes.bin")
+            arguments = [privbayes, "real", str(n), "1", str(self.theta)]
+            LOGGER.info('Calling %s', ' '.join(arguments))
+            start = datetime.utcnow()
+            subprocess.call(arguments, cwd=tmpdir)
+            LOGGER.info('Elapsed %s', datetime.utcnow() - start)
+
+            return np.loadtxt(
+                tmpdir / "output/syn_real_eps10_theta{}_iter0.dat".format(self.theta))
