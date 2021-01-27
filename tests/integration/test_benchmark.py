c1b68abaedc2d8003347c8edc2b4a1d9c6bf50f9
Carles Sala <carles@pythiac.com>
2021-01-27 18:59:31 +0100

    Fix tests

--- tests/integration/test_benchmark.py
+++ tests/integration/test_benchmark.py
@@ -3,11 +3,14 @@ import sdgym
 
 def test_identity():
     output = sdgym.run(
-        synthesizers=['Identity', 'Uniform'],
-        datasets=['got_families', 'KRK_v1'],
+        synthesizers=['Identity', 'Independent', 'Uniform'],
+        datasets=['trains_v1', 'KRK_v1'],
     )
 
     assert not output.empty
     assert set(output['modality'].unique()) == {'single-table', 'multi-table'}
-    assert output[output.synthesizer == 'Identity'].score.mean() > 0.9
-    assert output[output.synthesizer == 'Uniform'].score.mean() < 0.9
+
+    scores = output.groupby('synthesizer').score.mean().sort_values()
+
+    assert ['Uniform', 'Independent', 'Identity'] == scores.index.tolist()
+
