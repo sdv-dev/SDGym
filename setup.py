29e2d883ce41d47a2e5efc4729f0a38442cf1ef9
Carles Sala <carles@pythiac.com>
2021-06-14 18:26:00 +0200

    Bump version: 0.3.2.dev0 → 0.4.0.dev0

--- setup.py
+++ setup.py
@@ -119,6 +119,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDGym',
-    version='0.3.2.dev0',
+    version='0.4.0.dev0',
     zip_safe=False,
 )
