4b4413aa36328270091ed099e773cf157b6ccf04
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2021-06-17 09:48:46 -0700

    Bump version: 0.4.0.dev2 → 0.4.0

--- sdgym/__init__.py
+++ sdgym/__init__.py
@@ -8,7 +8,7 @@ __author__ = 'MIT Data To AI Lab'
 __copyright__ = 'Copyright (c) 2018, MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com'
 __license__ = 'MIT'
-__version__ = '0.4.0.dev2'
+__version__ = '0.4.0'
 
 from sdgym import benchmark, synthesizers
 from sdgym.benchmark import run
