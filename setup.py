3e5a25754a6fa6b19ab655ee88dfb8935dde9b1b
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2021-12-13 16:36:41 -0500

    Bump version: 0.5.0 → 0.5.1.dev0

--- setup.py
+++ setup.py
@@ -134,6 +134,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDGym',
-    version='0.5.0',
+    version='0.5.1.dev0',
     zip_safe=False,
 )
