7c20c588a4c9f5940885467406e73274a5b01a8e
Tejumade Afonja <tmafonja1@gmail.com>
2022-04-27 09:12:07 +0200

    sort import corretly

--- sdgym/synthesizers/__init__.py
+++ sdgym/synthesizers/__init__.py
@@ -5,8 +5,8 @@ from sdgym.synthesizers.independent import Independent
 from sdgym.synthesizers.medgan import MedGAN
 from sdgym.synthesizers.privbn import PrivBN
 from sdgym.synthesizers.sdv import (
-    CTGAN, TVAE, CopulaGAN, GaussianCopulaCategorical, GaussianCopulaCategoricalFuzzy,
-    GaussianCopulaOneHot, HMA1, PAR)
+    CTGAN, HMA1, PAR, TVAE, CopulaGAN, GaussianCopulaCategorical, GaussianCopulaCategoricalFuzzy,
+    GaussianCopulaOneHot)
 from sdgym.synthesizers.tablegan import TableGAN
 from sdgym.synthesizers.uniform import Uniform
 from sdgym.synthesizers.veegan import VEEGAN
