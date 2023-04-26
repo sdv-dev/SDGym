87f8b30ba4aeb94c9d2d7ecb4cae314afee94445
Felipe <fealho@gmail.com>
2023-04-26 15:18:00 +0100

    Fix lint

--- tests/unit/test_datasets.py
+++ tests/unit/test_datasets.py
@@ -7,7 +7,7 @@ import botocore
 import pandas as pd
 
 from sdgym.datasets import (
-    download_dataset, _get_bucket_name, _get_dataset_path, get_available_datasets,
+    _get_bucket_name, _get_dataset_path, download_dataset, get_available_datasets,
     get_dataset_paths)
 
 
