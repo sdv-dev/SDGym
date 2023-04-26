771c27ccb4e8e7871320222e9fffd60b2a0fa21a
Felipe <fealho@gmail.com>
2023-04-26 17:25:39 +0100

    Loosen test bounds

--- tests/integration/test_benchmark.py
+++ tests/integration/test_benchmark.py
@@ -177,10 +177,10 @@ def test_benchmark_single_table():
     assert set(results['Dataset']) == {'student_placements'}
     assert set(results['Dataset_Size_MB']) == {0.026358}
     assert results['Train_Time'].between(0, 100).all()
-    assert results['Peak_Memory_MB'].between(0, 20).all()
-    assert results['Synthesizer_Size_MB'].between(0, 10).all()
-    assert results['Sample_Time'].between(0, 1).all()
-    assert results['Evaluate_Time'].between(1, 5).all()
+    assert results['Peak_Memory_MB'].between(0, 100).all()
+    assert results['Synthesizer_Size_MB'].between(0, 100).all()
+    assert results['Sample_Time'].between(0, 10).all()
+    assert results['Evaluate_Time'].between(0, 10).all()
     assert results['Quality_Score'].between(.7, 1).all()
     assert results['NewRowSynthesis'][2] == 0
 
