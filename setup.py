6be34fc1075ebc7f0817d1d6eeb3ae2c305f6b5b
Felipe <fealho@gmail.com>
2023-08-14 09:54:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,9 +33,9 @@ install_requires = [
     "torch>=2.0.0;python_version>='3.11'",
     'tqdm>=4.15,<5',
     'XlsxWriter>=1.2.8,<4',
-    'rdt>=1.3.0,<2.0',
-    'sdmetrics>=0.9.1,<1.0',
-    'sdv>=1.0.0,<2',
+    'rdt>=1.6.1,<2.0',
+    'sdmetrics>=0.11.0,<1.0',
+    'sdv>=1.3.0.dev1,<2',
 ]
 
 
