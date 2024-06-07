64a3143af086437ffb259b294a97c8d6a53f4ece
Andrew Montanez <andrew@sdv.dev>
2024-06-07 10:09:56 -0500

    Bump version: 0.8.0.dev2 → 0.8.0

--- sdgym/__init__.py
+++ sdgym/__init__.py
@@ -8,7 +8,7 @@ __author__ = 'DataCebo, Inc.'
 __copyright__ = 'Copyright (c) 2022 DataCebo, Inc.'
 __email__ = 'info@sdv.dev'
 __license__ = 'BSL-1.1'
-__version__ = '0.8.0.dev2'
+__version__ = '0.8.0'
 
 import logging
 
