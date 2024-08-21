b3bbf4e309b9fe9d6b576a81fb475a415c0f2c9a
Felipe <fealho@gmail.com>
2024-08-20 19:17:41 -0700

    Clarify test

--- issue.py
+++ issue.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sdgym.benchmark import benchmark_single_table
 from sdgym.synthesizers.generate import create_single_table_synthesizer
 from sdv.metadata.single_table import SingleTableMetadata
@@ -9,16 +10,30 @@ def get_trained_synth(data, metadata):
     synthesizer.fit(data)
     return synthesizer
 
+def get_trained_synth_v2(data, metadata):
+    metadata = SingleTableMetadata.load_from_dict(metadata)
+    synthesizer = GaussianCopulaSynthesizer(metadata)
+    synthesizer.fit(data)
+    return synthesizer
+
 def sample_synth(trained_synthesizer, num_samples):
     return trained_synthesizer.sample(num_samples)
 
-custom_synthesizer = create_single_table_synthesizer('SimpleGaussianCopula', get_trained_synth, sample_synth)
+if __name__ == '__main__':
+    custom_synthesizer = create_single_table_synthesizer('SimpleGaussianCopula', get_trained_synth, sample_synth)
+    custom_synthesizer_v2 = create_single_table_synthesizer('SimpleGaussianCopulaV2', get_trained_synth_v2, sample_synth)
+    output = benchmark_single_table(
+        synthesizers=[],
+        sdv_datasets=['fake_hotel_guests'],
+        timeout=120,
+        sdmetrics=[],
+        custom_synthesizers=[custom_synthesizer, custom_synthesizer_v2],
+    )
 
-output = benchmark_single_table(
-    synthesizers=[],
-    sdv_datasets=['fake_hotel_guests'],
-    timeout=120,
-    sdmetrics=[],
-    custom_synthesizers=[custom_synthesizer],
-)
-print(output)
+    assert output['Synthesizer'][0] == 'Custom:SimpleGaussianCopula'
+    assert output['Synthesizer'][1] == 'Custom:SimpleGaussianCopulaV2'
+    output = output.drop('Quality_Score', axis=1)
+    assert not output.isna().to_numpy().any()
+
+    with pd.option_context('display.max_columns', None):
+        print(output)
