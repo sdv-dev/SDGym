7303181c2e85dbc94f37762031959f8562dffef7
Gaurav Sheni <gvsheni@gmail.com>
2025-06-12 14:34:56 -0400

    Add modality parameter to get_available_datasets function (#409)

---
+++ tests/integration/test_datasets.py
@@ -0,0 +1,23 @@
+import re
+
+import pytest
+
+from sdgym import get_available_datasets
+
+
+def test_get_available_datasets():
+    # Run
+    df = get_available_datasets('single_table')
+
+    # Assert
+    assert df.columns.tolist() == ['dataset_name', 'size_MB', 'num_tables']
+    assert df['num_tables'].unique().tolist() == [1]
+
+
+def test_get_available_datasets_raises():
+    # Setup
+    msg = re.escape("'modality' must be in ['single_table'].")
+
+    # Run and Assert
+    with pytest.raises(ValueError, match=msg):
+        get_available_datasets('multi-table')
