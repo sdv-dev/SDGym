c3d02f3e2080378a09b173bb0deb06d3bdee7a5c
R-Palazzo <romainpalazzo@gmail.com>
2025-07-15 12:56:36 +0200

    def 412

--- sdgym/sdgym_result_explorer/result_handler.py
+++ sdgym/sdgym_result_explorer/result_handler.py
@@ -8,6 +8,8 @@ from abc import ABC, abstractmethod
 import pandas as pd
 from botocore.exceptions import ClientError
 
+SYNTHESIZER_BASELINE = 'GaussianCopulaSynthesizer'
+
 
 class ResultsHandler(ABC):
     """Abstract base class for handling results storage and retrieval."""
@@ -64,6 +66,65 @@ class LocalResultsHandler(ResultsHandler):
     def load_synthetic_data(self, file_path):
         """Load synthetic data from a CSV file."""
         return pd.read_csv(os.path.join(self.base_path, file_path))
+
+    def _get_results_files(self, folder_name):
+        return [
+            f for f in os.listdir(os.path.join(self.base_path, folder_name))
+            if f.endswith('.csv') and f.startswith('results_')
+        ]
+
+    def _get_results(self, file_names):
+        return [
+            pd.read_csv(os.path.join(self.base_path, file_name)) for file_name in file_names
+        ]
+
+    def _compute_wins(self, result):
+        synthesizers = result['Synthesizer'].unique()
+        datasets = result['Dataset'].unique()
+        for synthesizer in synthesizers:
+            for dataset in datasets:
+                loc_synthesizer = (result['Synthesizer'] == synthesizer) & (result['Dataset'] == dataset)
+                score_synthesizer = result.loc[loc_synthesizer]['Quality_Score']
+                score_baseline = result.loc[(result['Synthesizer'] == SYNTHESIZER_BASELINE) & (result['Dataset'] == dataset)]['Quality_Score']
+                result.loc[loc_synthesizer, 'Win'] = score_synthesizer.values > score_baseline.values
+
+    def _get_summarize_table(self, folder_to_results):
+        """Create a summary table from the results."""
+        summarized_results = pd.DataFrame()
+        for folder, results in folder_to_results.items():
+            summarized_results[folder] = results.groupby(['Synthesizer'])['Win'].sum()
+
+        summarized_results = summarized_results.fillna('-')
+        return summarized_results
+
+    def summarize(self, folder_name):
+        """Summarize the results in the specified folder."""
+        date = pd.to_datetime(folder_name[-10:])
+        other_folders = [
+            f for f in os.listdir(self.base_path) if f.startswith(folder_name[:-11])
+        ]
+        all_folder = other_folders + [folder_name]
+        folder_to_results = {}
+        for folder in all_folder:
+            folder_date = pd.to_datetime(folder[-10:])
+            if folder_date > date:
+                continue
+
+            result_filenames = self._get_results_files(folder)
+            if not result_filenames:
+                continue
+
+            results = self._get_results(result_filenames)
+            if not results:
+                continue
+
+            aggregated_results = pd.concat(results, ignore_index=True)
+            aggregated_results = aggregated_results.drop_duplicates(subset=['dataset_name', 'synthesizer_name'])
+            self._compute_wins(aggregated_results)
+            folder_to_results[folder] = aggregated_results
+
+        summarized_table = self._get_summarize_table(folder_to_results)
+        return summarized_table
 
 
 class S3ResultsHandler(ResultsHandler):
