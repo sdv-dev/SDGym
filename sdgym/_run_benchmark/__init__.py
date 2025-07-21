575dddbf50b387e473f43198a87f8ab42a97088f
R-Palazzo <romainpalazzo@gmail.com>
2025-07-21 17:44:16 +0100

    trigger on pushes

--- sdgym/_run_benchmark/__init__.py
+++ sdgym/_run_benchmark/__init__.py
@@ -1,5 +1,5 @@
 """Folder for the SDGym benchmark module."""
 
-OUTPUT_DESTINATION_AWS = 's3://sdgym-benchmark/Debug/Issue_425/'
-UPLOAD_DESTINATION_AWS = 's3://sdgym-benchmark/Debug/Issue_425/'
+OUTPUT_DESTINATION_AWS = 's3://sdgym-benchmark/Debug/Issue_425_2/'
+UPLOAD_DESTINATION_AWS = 's3://sdgym-benchmark/Debug/Issue_425_2/'
 RESULTS_UPLOADED = False
