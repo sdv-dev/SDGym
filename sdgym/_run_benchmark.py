af7068955e80f389c4a186e4f56b783d53f64c4c
R-Palazzo <romainpalazzo@gmail.com>
2025-07-21 13:57:43 +0100

    def 425

---
+++ sdgym/_run_benchmark.py
@@ -0,0 +1,17 @@
+import os
+from sdgym.benchmark import benchmark_single_table_aws, SDV_SINGLE_TABLE_SYNTHESIZERS
+aws_key = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret = os.getenv('AWS_SECRET_ACCESS_KEY')
+OUTPUT_DESTINATION_AWS = 's3://sdgym-benchmark/Debug/Issue_425'
+synthesizer = SDV_SINGLE_TABLE_SYNTHESIZERS
+datasets = ['expedia_hotel_logs', 'child']
+    
+
+if __name__ == '__main__':
+    for synthesizer in ['GaussianCopulaSynthesizer', 'TVAESynthesizer']:
+        benchmark_single_table_aws(
+            output_destination=OUTPUT_DESTINATION_AWS,
+            aws_access_key_id=aws_key, aws_secret_access_key=aws_secret,
+            synthesizers=[synthesizer],
+            sdv_datasets=datasets,
+        )
