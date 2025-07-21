c9263010a6f5ab0301427ce273a2253cf9777c80
R-Palazzo <romainpalazzo@gmail.com>
2025-07-21 17:32:39 +0100

    define upload workflow

---
+++ sdgym/_run_benchmark.py/__init__.py
@@ -0,0 +1,5 @@
+"""Folder for the SDGym benchmark module."""
+
+OUTPUT_DESTINATION_AWS = 's3://sdgym-benchmark/Debug/Issue_425/'
+UPLOAD_DESTINATION_AWS = 's3://sdgym-benchmark/Debug/Issue_425/'
+RESULTS_UPLOADED = False
--- sdgym/_run_benchmark.py/run_benchmark.py
+++ sdgym/_run_benchmark.py/run_benchmark.py
@@ -1,19 +1,21 @@
 import os
 
 from sdgym.benchmark import SDV_SINGLE_TABLE_SYNTHESIZERS, benchmark_single_table_aws
+from sdgym.run_benchmark import OUTPUT_DESTINATION_AWS, RESULT_UPLOADED
 
 aws_key = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret = os.getenv('AWS_SECRET_ACCESS_KEY')
-OUTPUT_DESTINATION_AWS = 's3://sdgym-benchmark/Debug/Issue_425/'
 synthesizer = SDV_SINGLE_TABLE_SYNTHESIZERS
 datasets = ['expedia_hotel_logs', 'fake_companies']
 
 
 if __name__ == '__main__':
+    RESULT_UPLOADED = False
     for synthesizer in ['GaussianCopulaSynthesizer', 'TVAESynthesizer']:
         benchmark_single_table_aws(
             output_destination=OUTPUT_DESTINATION_AWS,
-            aws_access_key_id=aws_key, aws_secret_access_key=aws_secret,
+            aws_access_key_id=aws_key,
+            aws_secret_access_key=aws_secret,
             synthesizers=[synthesizer],
             sdv_datasets=datasets,
         )
