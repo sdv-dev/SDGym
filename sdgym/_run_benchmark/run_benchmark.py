03c47f26814106db641e43f72028d7a5ed420f03
R-Palazzo <romainpalazzo@gmail.com>
2025-07-21 18:20:41 +0100

    fix run workflow

--- sdgym/_run_benchmark/run_benchmark.py
+++ sdgym/_run_benchmark/run_benchmark.py
@@ -1,19 +1,17 @@
 import os
 
-from sdgym._run_benchmark import OUTPUT_DESTINATION_AWS, RESULTS_UPLOADED
-from sdgym.benchmark import SDV_SINGLE_TABLE_SYNTHESIZERS, benchmark_single_table_aws
+import sdgym._run_benchmark as run_benchmark
+from sdgym.benchmark import benchmark_single_table_aws
 
 aws_key = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret = os.getenv('AWS_SECRET_ACCESS_KEY')
-synthesizer = SDV_SINGLE_TABLE_SYNTHESIZERS
 datasets = ['expedia_hotel_logs', 'fake_companies']
 
-
 if __name__ == '__main__':
-    RESULTS_UPLOADED = False
+    run_benchmark.RESULTS_UPLOADED = False
     for synthesizer in ['GaussianCopulaSynthesizer', 'TVAESynthesizer']:
         benchmark_single_table_aws(
-            output_destination=OUTPUT_DESTINATION_AWS,
+            output_destination=run_benchmark.OUTPUT_DESTINATION_AWS,
             aws_access_key_id=aws_key,
             aws_secret_access_key=aws_secret,
             synthesizers=[synthesizer],
