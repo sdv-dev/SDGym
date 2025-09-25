b4a267d57ffcc6f345e8fab995ffeb5070ca38d5
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2025-09-25 16:27:24 +0200

    Update installable command for EC2

--- sdgym/benchmark.py
+++ sdgym/benchmark.py
@@ -1316,7 +1316,7 @@ def _get_user_data_script(access_key, secret_key, region_name, script_content):
 
         echo "======== Install Dependencies in venv ============"
         pip install --upgrade pip
-        pip install "sdgym[all] @ git+https://github.com/sdv-dev/SDGym.git@main"
+        pip install sdgym[all]
         pip install s3fs
 
         echo "======== Write Script ==========="
