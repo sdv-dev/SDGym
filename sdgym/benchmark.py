3c72c564d4d4471561104c6754e1b927c389087c
R-Palazzo <romainpalazzo@gmail.com>
2025-10-06 12:34:40 +0200

    fix tests + point to main

--- sdgym/benchmark.py
+++ sdgym/benchmark.py
@@ -1364,7 +1364,7 @@ def _get_user_data_script(access_key, secret_key, region_name, script_content):
 
         echo "======== Install Dependencies in venv ============"
         pip install --upgrade pip
-        pip install "sdgym[all] @ git+https://github.com/sdv-dev/SDGym.git@issue-438-fix-benchmark-aws"
+        pip install "sdgym[all] @ git+https://github.com/sdv-dev/SDGym.git@main"
         pip install s3fs
 
         echo "======== Write Script ==========="
