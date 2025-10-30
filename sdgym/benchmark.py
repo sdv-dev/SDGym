7e086dc68de8d7dd5da881474ecb1050619d9fc7
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2025-10-30 23:35:46 +0100

    Update AWS install

--- sdgym/benchmark.py
+++ sdgym/benchmark.py
@@ -1409,7 +1409,7 @@ def _get_user_data_script(access_key, secret_key, region_name, script_content):
 
         echo "======== Install Dependencies in venv ============"
         pip install --upgrade pip
-        pip install sdgym[all]
+        pip install "sdgym[all] @ git+https://github.com/sdv-dev/SDGym.git@issue-468-update-sdgym-to-use-the-new-s3-bucket-and-bucket-structure"
         pip install s3fs
 
         echo "======== Write Script ==========="
