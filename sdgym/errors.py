a9e999d958c9098adbeaec9d6f2d793e2f7acf3f
R-Palazzo <romainpalazzo@gmail.com>
2025-11-14 16:55:47 +0000

    Fix time propagation

--- sdgym/errors.py
+++ sdgym/errors.py
@@ -7,3 +7,29 @@ class SDGymError(Exception):
 
 class UnsupportedDataset(SDGymError):
     """The Dataset is not supported by this Synthesizer."""
+
+
+class SynthesisRunError(RuntimeError):
+    """Error to raise when there is an error during the benchmark."""
+
+    def __init__(
+        self,
+        *,
+        original_exc,
+        synthetic_data,
+        train_time,
+        sample_time,
+        synthesizer_size,
+        peak_memory,
+        exception_text,
+        error_text,
+    ):
+        super().__init__(str(original_exc))
+        self.original_exc = original_exc
+        self.synthetic_data = synthetic_data
+        self.train_time = train_time
+        self.sample_time = sample_time
+        self.synthesizer_size = synthesizer_size
+        self.peak_memory = peak_memory
+        self.exception = exception_text
+        self.error = error_text
