baeaf5d2ca05e67be65204bf525ef4f193fa5a5e
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2025-11-26 17:58:23 +0100

    Add unit tests for _dataset_utils.py

--- sdgym/_dataset_utils.py
+++ sdgym/_dataset_utils.py
@@ -7,6 +7,8 @@ from zipfile import ZipFile
 
 import numpy as np
 import pandas as pd
+from sdv.metadata import Metadata
+from sdv.utils import poc
 
 LOGGER = logging.getLogger(__name__)
 
@@ -23,6 +25,68 @@ def _parse_numeric_value(value, dataset_name, field_name, target_type=float):
         return np.nan
 
 
+def _get_multi_table_dataset_subset(data, metadata_dict):
+    """Create a smaller, referentially consistent subset of multi-table data.
+
+    This function limits each table to at most 10 columns by keeping all
+    mandatory columns and, if needed, a subset of the remaining columns, then
+    trims the underlying DataFrames to match the updated metadata. Finally, it
+    uses SDV's multi-table utility to sample up to 1,000 rows from
+    the main table and a consistent subset of rows from all related tables
+    while preserving referential integrity.
+
+    Args:
+        data (dict):
+            A dictionary where keys are table names and values are DataFrames
+            representing tables.
+        metadata_dict (dict):
+            Metadata dictionary containing schema information for each table.
+
+    Returns:
+        tuple:
+            A tuple containing:
+                - dict: The subset of the input data with reduced columns and rows.
+                - dict: The updated metadata dictionary reflecting the reduced column sets.
+    """
+    metadata = Metadata.load_from_dict(metadata_dict)
+    for table_name, table in metadata.tables.items():
+        all_columns = list(table.columns)
+        mandatory = metadata._get_all_keys(table_name)
+        optional_columns = [column for column in all_columns if column not in mandatory]
+        if len(mandatory) >= 10:
+            keep_columns = mandatory
+        else:
+            extra = 10 - len(mandatory)
+            keep_columns = mandatory + optional_columns[:extra]
+
+        # Filter only the subset of columns that will be used
+        subset_column_schema = {
+            column_name: table.columns[column_name]
+            for column_name in keep_columns
+            if column_name in table.columns
+        }
+        # Replace the columns with only the subset ones
+        metadata_dict['tables'][table_name]['columns'] = subset_column_schema
+
+    # Re-load the metadata object that will be used with the `SDV` utility function
+    metadata = Metadata.load_from_dict(metadata_dict)
+    largest_table_name = max(data, key=lambda table_name: len(data[table_name]))
+
+    # Trim the data to contain only the subset of columns
+    for table_name, table in metadata.tables.items():
+        data[table_name] = data[table_name][list(table.columns)]
+
+    # Subsample the data mantaining the referential integrity
+    data = poc.get_random_subset(
+        data=data,
+        metadata=metadata,
+        main_table_name=largest_table_name,
+        num_rows=1000,
+        verbose=False,
+    )
+    return data, metadata_dict
+
+
 def _get_dataset_subset(data, metadata_dict, modality):
     """Limit the size of a dataset for faster evaluation or testing.
 
@@ -31,25 +95,21 @@ def _get_dataset_subset(data, metadata_dict, modality):
     columns—such as sequence indices and keys in sequential datasets—are always retained.
 
     Args:
-        data (pd.DataFrame):
+        data (pd.DataFrame or dict):
             The dataset to be reduced.
         metadata_dict (dict):
-            A dictionary containing the dataset's metadata.
+            A dictionary representing the dataset's metadata.
         modality (str):
-            The dataset modality. Must be one of: ``'single_table'``, ``'sequential'``.
+            The dataset modality.
 
     Returns:
         tuple[pd.DataFrame, dict]:
             A tuple containing:
-            - The reduced dataset as a DataFrame.
+            - The reduced dataset as a DataFrame or Dictionary.
             - The updated metadata dictionary reflecting any removed columns.
-
-    Raises:
-        ValueError:
-            If the provided modality is ``'multi_table'``.
     """
     if modality == 'multi_table':
-        raise ValueError('limit_dataset_size is not supported for multi-table datasets.')
+        return _get_multi_table_dataset_subset(data, metadata_dict)
 
     max_rows, max_columns = (1000, 10)
     tables = metadata_dict.get('tables', {})
@@ -76,6 +136,7 @@ def _get_dataset_subset(data, metadata_dict, modality):
         }
 
     data = data[list(keep_columns)]
+    max_rows = min(1000, len(data))
     data = data.sample(max_rows)
     return data, metadata_dict
 
