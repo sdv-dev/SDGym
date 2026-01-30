2052ada5c21432ec9dce0eb2fee570fbc6750204
R-Palazzo <romainpalazzo@gmail.com>
2026-01-30 17:47:05 +0000

    test slack message

--- sdgym/run_benchmark/utils.py
+++ sdgym/run_benchmark/utils.py
@@ -12,7 +12,7 @@ from sdgym.s3 import parse_s3_path
 
 OUTPUT_DESTINATION_AWS = 's3://sdgym-benchmark/Benchmarks/'
 DEBUG_SLACK_CHANNEL = 'sdv-alerts-debug'
-SLACK_CHANNEL = 'sdv-alerts'
+SLACK_CHANNEL = 'sdv-alerts-debug' #'sdv-alerts'
 KEY_DATE_FILE = '_BENCHMARK_DATES.json'
 PLOTLY_MARKERS = [
     'circle',
