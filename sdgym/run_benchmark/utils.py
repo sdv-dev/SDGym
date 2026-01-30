1230aa93c4c2f5ccb56c4d658068e872730766e0
R-Palazzo <romainpalazzo@gmail.com>
2026-01-30 14:06:37 +0000

    update pyproject to include yaml file

--- sdgym/run_benchmark/utils.py
+++ sdgym/run_benchmark/utils.py
@@ -12,7 +12,7 @@ from sdgym.s3 import parse_s3_path
 
 OUTPUT_DESTINATION_AWS = 's3://sdgym-benchmark/Debug/Benchmarks/'
 DEBUG_SLACK_CHANNEL = 'sdv-alerts-debug'
-SLACK_CHANNEL = 'sdv_alerts-debug'  # 'sdv-alerts'
+SLACK_CHANNEL = 'sdv-alert-debug'  # 'sdv-alerts'
 KEY_DATE_FILE = '_BENCHMARK_DATES.json'
 PLOTLY_MARKERS = [
     'circle',
