b33605c55e5ebf5f0aed31a288088118ec66286e
R-Palazzo <116157184+R-Palazzo@users.noreply.github.com>
2026-03-17 12:16:16 +0000

    Move the current benchmark configs to yaml files (#566)

---
+++ sdgym/_benchmark_launcher/__init__.py
@@ -0,0 +1,6 @@
+"""Benchmark Launcher Module."""
+
+from sdgym._benchmark_launcher.benchmark_config import BenchmarkConfig
+from sdgym._benchmark_launcher.benchmark_launcher import BenchmarkLauncher
+
+__all__ = ('BenchmarkConfig', 'BenchmarkLauncher')
