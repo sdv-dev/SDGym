b642484f1083b1c2d93a1bb961f396a88005a2a0
R-Palazzo <romainpalazzo@gmail.com>
2026-05-05 14:50:42 +0100

    fix

--- sdgym/_benchmark/benchmark.py
+++ sdgym/_benchmark/benchmark.py
@@ -206,7 +206,7 @@ def _get_user_data_script(
         log "======== Install Dependencies =========="
         pip install --upgrade pip
         {sdv_install}
-        pip install "sdgym[all] @ git+https://github.com/sdv-dev/SDGym.git@rel_bench_datasets"
+        pip install "sdgym[all] @ git+https://github.com/sdv-dev/SDGym.git@rel-bench-datasets"
 
         {gpu_block}
 
