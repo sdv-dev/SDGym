a88e0b18ea9552a112c7c722ce762b5737c756c4
R-Palazzo <romainpalazzo@gmail.com>
2026-05-28 11:30:47 +0100

    undo changes for testing

--- sdgym/_benchmark/benchmark.py
+++ sdgym/_benchmark/benchmark.py
@@ -206,7 +206,7 @@ def _get_user_data_script(
         log "======== Install Dependencies =========="
         pip install --upgrade pip
         {sdv_install}
-        pip install "sdgym[all] @ git+https://github.com/sdv-dev/SDGym.git@issue-605-single-job-per-instance"
+        pip install "sdgym[all]"
 
         {gpu_block}
 
